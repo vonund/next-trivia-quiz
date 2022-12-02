1f6939c651938635a77a8dcf8f4dc1fb8aa9d58c
vonund <vonund@gmail.com>
2022-12-02 18:46:21 +0200

    feat: add static data and set up server props

---
+++ src/pages/index.tsx
@@ -0,0 +1,45 @@
+import type { GetServerSideProps, NextPage } from "next";
+
+const CATEGORIES = {
+  "Arts & Literature": ["arts", "literature", "arts_and_literature"],
+  "Film & TV": ["movies", "film", "film_and_tv"],
+  "Food & Drink": ["food_and_drink", "food", "drink"],
+  "General Knowledge": ["general_knowledge"],
+  Geography: ["geography"],
+  History: ["history"],
+  Music: ["music"],
+  Science: ["science"],
+  "Society & Culture": ["society_and_culture", "society", "culture"],
+  "Sport & Leisure": ["sport_and_leisure", "sports", "sport"],
+};
+
+interface CategoryInterface {
+  [key: string]: string[];
+}
+
+type PropsType = {
+  categories: CategoryInterface | null;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const props: PropsType = {
+    categories: CATEGORIES,
+  };
+
+  return {
+    props,
+  };
+};
+
+const Home: NextPage<PropsType> = ({ categories }) => {
+  console.log(categories);
+
+  return (
+    <>
+      <header></header>
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
